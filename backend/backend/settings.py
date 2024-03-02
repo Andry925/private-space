d2d5545c84ac7c4ccd36c607c220ff407a7186a0
andrew <andry925@ukr.net>
2024-03-02 16:24:34 +0200

    Adding admin page

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,13 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'rest_framework',
-    'corsheaders',
+    "corsheaders",
 
 
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
