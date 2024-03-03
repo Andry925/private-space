d2410698b6ce6791573170acbccd03ba608bb96d
andrew <andry925@ukr.net>
2024-03-03 04:32:18 +0200

    Docker finished

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('', include('subject.urls'))
+    path('api/', include('accounts.urls')),
+    path('api/', include('subject.urls'))
 
 ]
--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -7,110 +7,98 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 import './Signup.css';
+axios.defaults.withCredentials = true;
 
 const Signup = () => {
-    const navigate = useNavigate();
-    const [name, setName] = useState('');
-    const [surname, setSurname] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [selectedRole, setSelectedRole] = useState(null);
-    const [dropdownVisible, setDropdownVisible] = useState(false);
+  const navigate = useNavigate();
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setSurname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [role, setSelectedRole] = useState(null);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handleSurnameChange = (e) => {
-        setSurname(e.target.value);
-    };
+  const registration = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/register/", {
+        email,
+        first_name,
+        last_name,
+        role,
+        password,
+      });
 
-    const handleEmailChange = (e) => {
-        const value = e.target.value;
-        setEmail(value);
-
-        if (!value.endsWith('@cosmic.edu')) {
-            setErrorMessage('Hey, it should be cosmic email :)');
-        } else {
-            setErrorMessage('');
-        }
-    };
-
-    const toggleDropdown = () => {
-        setDropdownVisible(!dropdownVisible);
-    };
-
-    const handleLogin = async () => {
-        try {
-          const response = await axios.post("http://127.0.0.1:8000/login", {
-            email,
-            password,
-          }, { withCredentials: true });
-    
-          if (response.status === 201 || response.status === 200) {
-              localStorage.setItem('sessionId', response.data.sessionId);
-            navigate('/c');
-          }
-        } catch (error) {
-          console.error("Such user does not exist", error);
-        }
+      if (response.status === 201 || response.status === 200) {
+        navigate('/login');
       }
+    } catch (error) {
+      console.error("Failed registration", error);
+      setErrorMessage("Registration failed. Please try again.");
+    }
 
-    const selectRole = (role) => {
-        setSelectedRole(role);
-        setDropdownVisible(false); 
-    };
+    console.log('Registration function called');
+  };
 
-    return (
-        <div className='login-container'>
-            <div className='login-items'>
-                <img src={cosmo} alt="Cosmo Logo" className='cosmoLogo'/>
-                <p className='login-caption'>Log in</p>
-                <div className='dropdown'>
-                    <p className='as'>as</p>
-                    <div className="dropdown">
-                    <button className="dropdown-btn" onClick={toggleDropdown}>
-                        {selectedRole ? selectedRole : 'Choose Role'}
-                    </button>
-                        {dropdownVisible && (
-                            <div className="dropdown-content" id="dropdownContent">
-                                <a href="#" onClick={() => selectRole('Teacher')}>Teacher</a>
-                                <a href="#" onClick={() => selectRole('Student')}>Student</a>
-                            </div>
-                        )}
-                    </div>
-                </div>
-                    <div className="inputs">
-                        <div className='ns-container'>
-                             <div className="ns-f1">
-                                <img src={user} alt="user"/>
-                                <input type="user" placeholder='Name' onChange={(e) => setEmail(e.target.value)}/>
-                            </div>
-                            <div className="ns">
-                                <img src={user} alt="user"/>
-                                <input type="user" placeholder='Surname' onChange={(e) => setEmail(e.target.value)}/>
-                            </div>
-                        </div>
-                           
-                        <div className="input">
-                            <img src={envelope} alt="email"/>
-                            <input type="email" placeholder='Cosmic E-mail' onChange={handleEmailChange} value={email} />
-                            {errorMessage && <p style={{ color: 'red', fontFamily: 'Montserrat', fontSize: '10px', fontWeight: '600' }}>{errorMessage}</p>}
-                        </div>
-                        <div className="input">
-                            <img src={lock} alt="lock"/>
-                            <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
-                        </div>
-                    </div>
-                    <div className="submit-container">
-                        <button
-                            className="submit">Let's go!
-                        </button>
-                    </div> 
-                </div>
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  const selectRole = (selectedRole) => {
+    setSelectedRole(selectedRole);
+    setDropdownVisible(false);
+  };
+
+  return (
+    <div className='login-container'>
+      <div className='login-items'>
+        <img src={cosmo} alt="Cosmo Logo" className='cosmoLogo' />
+        <p className='login-caption'>Log in</p>
+        <div className='dropdown'>
+          <p className='as'>as</p>
+          <div className="dropdown">
+            <button className="dropdown-btn" onClick={toggleDropdown}>
+              {role ? role : 'Choose Role'}
+            </button>
+            {dropdownVisible && (
+              <div className="dropdown-content" id="dropdownContent">
+                <a href="#" onClick={() => selectRole('Teacher')}>Teacher</a>
+                <a href="#" onClick={() => selectRole('Student')}>Student</a>
+              </div>
+            )}
+          </div>
         </div>
-    );
+        <div className="inputs">
+          <div className='ns-container'>
+            <div className="ns-f1">
+              <img src={user} alt="user" />
+              <input type="text" placeholder='First Name' onChange={(e) => setFirstName(e.target.value)} />
+            </div>
+            <div className="ns">
+              <img src={user} alt="user" />
+              <input type="text" placeholder='Surname' onChange={(e) => setSurname(e.target.value)} />
+            </div>
+          </div>
+          <div className="input">
+            <img src={envelope} alt="email" />
+            <input type="email" placeholder='Cosmic E-mail' onChange={handleEmailChange} value={email} />
+            {errorMessage && <p style={{ color: 'red', fontFamily: 'Montserrat', fontSize: '10px', fontWeight: '600' }}>{errorMessage}</p>}
+          </div>
+          <div className="input">
+            <img src={lock} alt="lock" />
+            <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+          </div>
+        </div>
+        <div className="submit-container">
+          <button className="submit" onClick={registration}>Реєстрація</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
