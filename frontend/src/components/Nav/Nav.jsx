ce8002c11e14516844e65b21e10719faa24f8977
Anastasiia Snihur <nastenasnigur@gmail.com>
2024-03-03 03:38:43 +0200

    ProfileInfo added

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Nav.css'
 
-const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
+function Nav() {
+    const [activeTab, setActiveTab] = useState('tab-profile');
+
+    const handleTabClick = (tabId) => {
+        setActiveTab(tabId);
+    };
+
+    return (
+        <div className="navbar">
+            <a href="/" className={activeTab === 'tab-profile' ? 'tab active' : 'tab'} id="tab-profile" onClick={() => handleTabClick('tab-profile')}>Profile</a>
+            <a href="/" className={activeTab === 'tab-home' ? 'tab active' : 'tab'} id="tab-home" onClick={() => handleTabClick('tab-home')}>Home</a>
+            <a href="/" className={activeTab === 'tab-test' ? 'tab active' : 'tab'} id="tab-test" onClick={() => handleTabClick('tab-test')}>Tests</a>
+            <a href="/" className={activeTab === 'tab-labs' ? 'tab active' : 'tab'} id="tab-labs" onClick={() => handleTabClick('tab-labs')}>Labs</a>
+            <a href="/" className={activeTab === 'tab-tasks' ? 'tab active' : 'tab'} id="tab-tasks" onClick={() => handleTabClick('tab-tasks')}>Tasks</a>
+            <a href="/" className={activeTab === 'tab-activities' ? 'tab active' : 'tab'} id="tab-activities" onClick={() => handleTabClick('tab-activities')}>Activities</a>
+        </div>
+    );
 }
 
-export default Nav
+export default Nav;
--- frontend/src/pages/CabinetPage/CabinetPage.js
+++ frontend/src/pages/CabinetPage/CabinetPage.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Nav from '../../components/Nav/Nav'
+import Cosmic from '../../components/Cosmic/Cosmic'
+import ProfileInfo from '../../components/ProfileInfo/ProfileInfo'
 
 const CabinetPage = () => {
   return (
     <div>
         <Nav/>
+        <div className='profile-container'>
+          <Cosmic/>
+          <ProfileInfo/>
+        </div>
     </div>
   )
 }
