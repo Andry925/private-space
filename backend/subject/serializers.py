974eab8542517c67c3dd5312362e89570d150796
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-03-03 03:16:26 +0200

    Adding endpoints for subjects (#7)

--- backend/subject/serializers.py
+++ backend/subject/serializers.py
@@ -7,3 +7,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject  # Set the model attribute to your Subject model
+        fields = '__all__'  # You can adjust this based on the fields you want to include in the serialization
--- backend/subject/urls.py
+++ backend/subject/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("all_profiles/", views.ViewAllProfilesView.as_view(), name="all_profiles"),
     path("single_profile/<int:pk>/", views.EditStudentProfileView.as_view(), name="single_profile"),
     path("edit/<int:pk>/", views.EditStudentProfileView.as_view(), name="edit_profile"),
+    path("single_subject/<int:pk>/", views.EditSubjectView.as_view(), name="single_subject"),
+    path("edit_subject/<int:pk>/", views.EditSubjectView.as_view(), name="edit_subject"),
 ]
